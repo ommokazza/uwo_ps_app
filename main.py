461451bba5ce5d06b1ddab9339e1257213af420f
ommokazza <ommokazza@gmail.com>
2018-09-26 02:39:16 +0900

    Implement 'Report Screenshot'

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import tkinter as tk
 from tkinter.filedialog import askdirectory
 
 from datetime import datetime
+from imgurpython import ImgurClient
 from pathlib import Path
 import pandas as pd
+import requests
+import threading
 import webbrowser
 
 from monitor import DirectoryMonitor
@@ -12,8 +15,12 @@ from formatter import FoxyFormatter
 
 class MainApp(tk.Tk):
     PAD = "7"
+    CLIENT_ID = '077727de8f6f20d'
+
     screenshot_dir = str(Path.home())\
                    + '\\Documents\\KOEI\\GV Online Eg\\ScreenShot'
+    last_screenshot = ""
+    reporting = False
 
     def __init__(self, estimator, formatter):
         tk.Tk.__init__(self)
@@ -31,19 +38,23 @@ class MainApp(tk.Tk):
 
     def __create_menus(self):
         self.menubar = tk.Menu(self, tearoff=0, relief="raised")
-        self.menubar.add("command", label="About", 
+        self.menubar.add("command", label="About",
                          command=self.menu_about, underline=0)
         self.menubar.add("separator")
         self.menubar.add("command", label="Select Directory",
                          command=self.menu_select_directory, underline=0)
+        #TODO
+        # self.menubar.add("separator")
+        # self.menubar.add("command", label="Clear Screenshots",
+        #                  command=self.menu_clear_screenshots, underline=0)
         self.menubar.add("separator")
         self.menubar.add("command", label="Report Screenshot",
                          command=self.menu_report_screenshot, underline=0)
         self.config(menu=self.menubar)
-    
+
     def __create_ui(self):
         result_frame = tk.Frame(self, relief="flat", bd=1)
-        result_frame.pack(side="top", 
+        result_frame.pack(side="top",
                           fill="x", expand=False)
 
         self.result_str = tk.StringVar()
@@ -73,13 +84,12 @@ class MainApp(tk.Tk):
         if not result:
             return
 
+        self.last_screenshot = path
+
         self.result_str.set(self.formatter.apply(result))
         self.copy_to_clipboard(None)
 
-        list_item = '(' + datetime.now().strftime('%H:%M') + ') '\
-                  + self.result_str.get()
-        self.list_box.insert(0, list_item)
-        self.list_box.see(0)
+        self.log(self.result_str.get())
         self.attributes('-topmost', 1)
         self.attributes('-topmost', 0)
 
@@ -92,12 +102,46 @@ class MainApp(tk.Tk):
             self.screenshot_dir = selected_dir
             self.monitor.set_path(self.screenshot_dir)
 
-    def menu_report_screenshot(self):
+    def menu_clear_screenshots(self):
         pass#TODO
+
+    def menu_report_screenshot(self):
+        if self.last_screenshot:
+            threading.Thread(target=self.__report_screenshot).start()
+        else:
+            self.log("There is no screenshot to report.")
+
+    def __report_screenshot(self):
+        if self.reporting:
+            self.log("Can not reporting for now.")
+            return
+        self.reporting = True
+        try:
+            self.log(">> Reporting started")
+            client = ImgurClient(self.CLIENT_ID, None)
+            config = {'album': "lP33mCsHgZrsO8K"}
+            image = client.upload_from_path(self.last_screenshot,
+                                            config=config)
+            deletehash = image['deletehash']
+            url = 'https://api.imgur.com/3/image/' + deletehash
+            headers = {'Authorization': 'Client-ID ' + self.CLIENT_ID}
+            requests.post(url, headers=headers, data={'description': deletehash})
+            self.log(image['link'])
+            self.log("<< Reporting completed" )
+        except:
+            self.log("<< Reporting Failed")
+        finally:
+            self.reporting = False
 
     def copy_to_clipboard(self, event):
         df = pd.DataFrame([self.result_str.get()])
         df.to_clipboard(index=False,header=False)
+
+    def log(self, msg):
+        size = self.list_box.size()
+        message = '(%s) %s' % (datetime.now().strftime('%H:%M:%S'), msg)
+        self.list_box.insert(size, message)
+        self.list_box.see(size)
 
 
 MODEL_DIRS = [
