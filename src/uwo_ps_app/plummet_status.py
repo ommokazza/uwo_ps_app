ac309ffafd5bc2e2d3b65ac6b207beec3f487cce
ommokazza <ommokazza@gmail.com>
2018-10-25 16:32:16 +0900

    Add function to check chatting messages

---
+++ src/uwo_ps_app/plummet_status.py
@@ -0,0 +1,82 @@
+import os
+
+from PIL import Image
+
+from uwo_ps_utils import common
+from uwo_ps_utils import cropper
+
+
+STATUS_WELCOME = "Welcome"
+STATUS_FLOODED = "Flooded"
+STATUS_PLUMMET = "Plummet"
+
+
+TH = 200    #Threshhold
+def __clear_bg(c):
+    return int(c / TH) * 255
+def __get_bytes(im):
+    return im.point(__clear_bg).tobytes()
+
+MK_BYTES = __get_bytes(Image.open("resources/chat_imgs/marketkeeper.png"))
+MA_BYTES = __get_bytes(Image.open("resources/chat_imgs/marketappentice.png"))
+FD_BYTES = __get_bytes(Image.open("resources/chat_imgs/flooded.png"))
+PM_BYTES = __get_bytes(Image.open("resources/chat_imgs/plummet.png"))
+WELCOMES = [
+    __get_bytes(Image.open("resources/chat_imgs/welcome1.png")),
+    __get_bytes(Image.open("resources/chat_imgs/welcome2.png")),
+    __get_bytes(Image.open("resources/chat_imgs/welcome3.png"))
+]
+
+def __init_categories_bytes():
+    categories = {}
+    for f in common.get_image_paths("resources/chat_imgs", ".png"):
+        basename = os.path.basename(f)
+        if not basename.startswith("Class_"):
+            continue
+        name = basename.replace("Class_", "").replace(".png", "")
+        categories[name] = __get_bytes(Image.open(f))
+
+    return categories
+
+CATEGORIES = __init_categories_bytes()
+
+def get_status(im):
+    for chat in cropper.get_chat_msg(im):
+        teller_bytes = cropper.get_teller(chat).point(__clear_bg).tobytes()
+        if MK_BYTES == teller_bytes or MA_BYTES == teller_bytes:
+            status = __get_status(chat)
+            if status == None:
+                continue
+            category = __get_category(chat)
+            return (status, category)
+
+    return None
+
+def __get_status(chat):
+    t1_bytes = __get_bytes(cropper.get_first_token(chat))
+    for welcome_bytes in WELCOMES:
+        if t1_bytes == welcome_bytes:
+            return STATUS_WELCOME
+
+    if t1_bytes == FD_BYTES:
+        return STATUS_FLOODED
+
+    if t1_bytes == PM_BYTES:
+        return STATUS_PLUMMET
+
+    return None
+
+def __get_category(chat):
+    t1_bytes = __get_bytes(cropper.get_first_token(chat))
+    if t1_bytes == FD_BYTES:
+        c_bytes = __get_bytes(cropper.get_flooded_second_token(chat))
+    elif t1_bytes == PM_BYTES:
+        c_bytes = __get_bytes(cropper.get_plummet_second_token(chat))
+    else:
+        return None
+
+    for category in CATEGORIES.keys():
+        if c_bytes == CATEGORIES[category]:
+            return category
+
+    return None
